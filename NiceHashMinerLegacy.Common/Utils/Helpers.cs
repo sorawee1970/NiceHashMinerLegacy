1a7fcce279c1f7a37dd52f95eca6fc8ed4ba878f
Dillon Newell <dillon_newell@live.ca>
2018-07-05 20:41:16 -0400

    move other methods to common

--- NiceHashMinerLegacy.Common/Utils/Helpers.cs
+++ NiceHashMinerLegacy.Common/Utils/Helpers.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using NiceHashMinerLegacy.Common.Configs;
+using NiceHashMinerLegacy.Common.Enums;
 
 namespace NiceHashMinerLegacy.Common.Utils
 {
-    public class Helpers
+    public static class Helpers
     {
         public static void ConsolePrint(string grp, string text)
         {
@@ -28,6 +30,86 @@ namespace NiceHashMinerLegacy.Common.Utils
         public static void ConsolePrint(string grp, string text, params object[] arg)
         {
             ConsolePrint(grp, string.Format(text, arg));
+        }
+
+        public static string FormatSpeedOutput(double speed, string separator = " ")
+        {
+            string ret;
+
+            if (speed < 1000)
+                ret = (speed).ToString("F3", CultureInfo.InvariantCulture) + separator;
+            else if (speed < 100000)
+                ret = (speed * 0.001).ToString("F3", CultureInfo.InvariantCulture) + separator + "k";
+            else if (speed < 100000000)
+                ret = (speed * 0.000001).ToString("F3", CultureInfo.InvariantCulture) + separator + "M";
+            else
+                ret = (speed * 0.000000001).ToString("F3", CultureInfo.InvariantCulture) + separator + "G";
+
+            return ret;
+        }
+
+        public static string FormatDualSpeedOutput(double primarySpeed, double secondarySpeed = 0, AlgorithmType algo = AlgorithmType.NONE)
+        {
+            string ret;
+            if (secondarySpeed > 0)
+            {
+                ret = FormatSpeedOutput(primarySpeed, "") + "/" + FormatSpeedOutput(secondarySpeed, "") + " ";
+            }
+            else
+            {
+                ret = FormatSpeedOutput(primarySpeed);
+            }
+            var unit = (algo == AlgorithmType.Equihash) ? "Sol/s " : "H/s ";
+            return ret + unit;
+        }
+
+        // parsing helpers
+
+        public static int ParseInt(string text)
+        {
+            return int.TryParse(text, out var tmpVal) ? tmpVal : 0;
+        }
+
+        public static long ParseLong(string text)
+        {
+            return long.TryParse(text, out var tmpVal) ? tmpVal : 0;
+        }
+
+        public static double ParseDouble(string text)
+        {
+            try
+            {
+                var parseText = text.Replace(',', '.');
+                return double.Parse(parseText, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static AlgorithmType DualAlgoFromAlgos(AlgorithmType primary, AlgorithmType secondary)
+        {
+            if (primary == AlgorithmType.DaggerHashimoto)
+            {
+                switch (secondary)
+                {
+                    case AlgorithmType.Decred:
+                        return AlgorithmType.DaggerDecred;
+                    case AlgorithmType.Lbry:
+                        return AlgorithmType.DaggerLbry;
+                    case AlgorithmType.Pascal:
+                        return AlgorithmType.DaggerPascal;
+                    case AlgorithmType.Sia:
+                        return AlgorithmType.DaggerSia;
+                    case AlgorithmType.Blake2s:
+                        return AlgorithmType.DaggerBlake2s;
+                    case AlgorithmType.Keccak:
+                        return AlgorithmType.DaggerKeccak;
+                }
+            }
+
+            return primary;
         }
     }
 }
