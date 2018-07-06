8774090b185cdd739d1dfb352b614b2ff4727558
Dillon Newell <dillon_newell@live.ca>
2018-07-05 20:38:02 -0400

    fix

--- NiceHashMiner/Miners/cpuminer.cs
+++ NiceHashMiner/Miners/cpuminer.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using NiceHashMiner.Algorithms;
 using NiceHashMiner.Miners.Parsing;
 using NiceHashMinerLegacy.Common.Enums;
+using NiceHashMinerLegacy.Common.Utils;
 
 namespace NiceHashMiner.Miners
 {
